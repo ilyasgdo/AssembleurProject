d67f7e3e6fa37a4e4dfb7a0d8b6a1cd47fbff85b
ilyas ghandaoui <127845273+ilyasgdo@users.noreply.github.com>
2025-01-08 11:24:10 +0100

    Update moteur.s

--- moteur.s
+++ moteur.s
@@ -1,8 +1,6 @@
-	;; RK - Evalbot (Cortex M3 de Texas Instrument); 
+;; RK - Evalbot (Cortex M3 de Texas Instrument); 
 ; programme - Pilotage 2 Moteurs Evalbot par PWM tout en ASM (configure les pwms + GPIO)
-
 ;Les pages se réfèrent au datasheet lm3s9b92.pdf
-
 ;Cablage :
 ;pin 10/PD0/PWM0 => input PWM du pont en H DRV8801RT
 ;pin 11/PD1/PWM1 => input Phase_R  du pont en H DRV8801RT
@@ -10,17 +8,14 @@
 ;pin 98/PD5		 => input Enable 12v du conv DC/DC 
 ;pin 86/PH0/PWM2 => input PWM du 2nd pont en H
 ;pin 85/PH1/PWM3 => input Phase du 2nd pont en H	
-
 ;; Hexa corresponding values to pin numbers
 GPIO_0		EQU		0x1
 GPIO_1		EQU		0x2
 GPIO_2		EQU		0x4
 GPIO_5		EQU		0x20
-
 ;; pour enable clock    0x400FE000
 SYSCTL_RCGC0	EQU		0x400FE100		;SYSCTL_RCGC0: offset 0x100 (p271 datasheet de lm3s9b92.pdf)
 SYSCTL_RCGC2	EQU		0x400FE108		;SYSCTL_RCGC2: offset 0x108 (p291 datasheet de lm3s9b92.pdf)
-
 ;; General-Purpose Input/Outputs (GPIO) configuration
 PORTD_BASE		EQU		0x40007000
 GPIODATA_D		EQU		PORTD_BASE
@@ -29,7 +24,6 @@ GPIODR2R_D		EQU		PORTD_BASE+0x00000500
 GPIODEN_D		EQU		PORTD_BASE+0x0000051C
 GPIOPCTL_D		EQU		PORTD_BASE+0x0000052C ; GPIO Port Control (GPIOPCTL), offset 0x52C; p444
 GPIOAFSEL_D		EQU		PORTD_BASE+0x00000420 ; GPIO Alternate Function Select (GPIOAFSEL), offset 0x420; p426
-
 PORTH_BASE		EQU		0x40027000
 GPIODATA_H		EQU		PORTH_BASE
 GPIODIR_H		EQU		PORTH_BASE+0x00000400
@@ -37,11 +31,9 @@ GPIODR2R_H		EQU		PORTH_BASE+0x00000500
 GPIODEN_H		EQU		PORTH_BASE+0x0000051C
 GPIOPCTL_H		EQU		PORTH_BASE+0x0000052C ; GPIO Port Control (GPIOPCTL), offset 0x52C; p444
 GPIOAFSEL_H		EQU		PORTH_BASE+0x00000420 ; GPIO Alternate Function Select (GPIOAFSEL), offset 0x420; p426
-
 ;; Pulse Width Modulator (PWM) configuration
 PWM_BASE		EQU		0x040028000 	   ;BASE des Block PWM p.1138
 PWMENABLE		EQU		PWM_BASE+0x008	   ; p1145
-
 ;Block PWM0 pour sorties PWM0 et PWM1 (moteur 1)
 PWM0CTL			EQU		PWM_BASE+0x040 ;p1167
 PWM0LOAD		EQU		PWM_BASE+0x050
@@ -49,7 +41,6 @@ PWM0CMPA		EQU		PWM_BASE+0x058
 PWM0CMPB		EQU		PWM_BASE+0x05C
 PWM0GENA		EQU		PWM_BASE+0x060
 PWM0GENB		EQU		PWM_BASE+0x064
-
 ;Block PWM1 pour sorties PWM1 et PWM2 (moteur 2)
 PWM1CTL			EQU		PWM_BASE+0x080 
 PWM1LOAD		EQU		PWM_BASE+0x090
@@ -57,9 +48,6 @@ PWM1CMPA		EQU		PWM_BASE+0x098
 PWM1CMPB		EQU		PWM_BASE+0x09C
 PWM1GENA		EQU		PWM_BASE+0x0A0
 PWM1GENB		EQU		PWM_BASE+0x0A4
-
-
-VITESSE			EQU		0x23	; Valeures plus petites => Vitesse plus rapide exemple 0x192
 							; Valeures plus grandes => Vitesse moins rapide exemple 0x1B2
 						
 						
@@ -78,14 +66,11 @@ VITESSE			EQU		0x23	; Valeures plus petites => Vitesse plus rapide exemple 0x192
 		EXPORT  MOTEUR_GAUCHE_AVANT
 		EXPORT  MOTEUR_GAUCHE_ARRIERE
 		EXPORT  MOTEUR_GAUCHE_INVERSE
-
-
 MOTEUR_INIT	
 		ldr r6, = SYSCTL_RCGC0
 		ldr	r0, [R6]
         ORR	r0, r0, #0x00100000  ;;bit 20 = PWM recoit clock: ON (p271) 
         str r0, [r6]
-
 	;ROM_SysCtlPWMClockSet(SYSCTL_PWMDIV_1);PWM clock is processor clock /1
 	;Je ne fais rien car par defaut = OK!!
 	;*(int *) (0x400FE060)= *(int *)(0x400FE060)...;
@@ -95,7 +80,6 @@ MOTEUR_INIT
 		ldr	r0, [R6] 		
         ORR	r0, r0, #0x08  ;; Enable port D GPIO 
         str r0, [r6]
-
 	;MOT2 : RCGC2 :  Enable port H GPIO  (2eme moteurs)
 		ldr r6, = SYSCTL_RCGC2
 		ldr	r0, [R6] 
@@ -126,7 +110,6 @@ MOTEUR_INIT
 		ldr	r0, [R6] 	  ;*(int *)(0x40007000+0x00000420)= *(int *)(0x40007000+0x00000420) | 0x00000001;
         ORR	r0, r0, #0x01 ;
         str r0, [r6]
-
 	;;MOT2 : Alternate Function Select (p 426), PH0 utilise PWM donc Alternate funct
 	;;donc PH0 = 1
 		ldr r6, = GPIOAFSEL_H
@@ -149,28 +132,26 @@ MOTEUR_INIT
 		ldr r6, =PWM0GENB;en comptage croissant, qd comparateurB = compteur => sortie pwmA=1
 		mov	r0,	#0x0B00	;en decomptage, qd comparateurB = compteur => sortie pwmB=0
 		str r0, [r6]	
-	;Config Compteur, comparateur A et comparateur B
-  	;;#define PWM_PERIOD (ROM_SysCtlClockGet() / 16000),
-	;;en mesure : SysCtlClockGet=0F42400h, /16=0x3E8, 
-	;;on divise par 2 car moteur 6v sur alim 12v
-		ldr	r6, =PWM0LOAD ;PWM0LOAD=periode/2 =0x1F4
-		mov r0,	#0x1F4
-		str	r0,[r6]
-		
-		ldr	r6, =PWM0CMPA ;Valeur rapport cyclique : pour 10% => 1C2h si clock = 0F42400
-		mov	r0, #VITESSE
-		str	r0, [r6]  
-		
-		ldr	r6, =PWM0CMPB ;PWM0CMPB recoit meme valeur. (rapport cyclique depend de CMPA)
-		mov	r0,	#0x1F4	
-		str	r0,	[r6]
-		
+		;; Initialisation du PWM pour le moteur
+	;; #define PWM_PERIOD (ROM_SysCtlClockGet() / 16000)
+	;; SysCtlClockGet = 0x0F42400h => période calculée = 0x3E8
+	;; Division par 2 car moteur 6V sur alimentation 12V
+
+		ldr	r6, =PWM0LOAD         ; Charger l'adresse de PWM0LOAD
+		mov	r0, #0x1F4           ; Période (divisée par 2)
+		str	r0, [r6]             ; Configurer la période
+
+		ldr	r6, =PWM0CMPA        ; Charger l'adresse de PWM0CMPA
+		mov	r0, r12             ; Charger la vitesse actuelle depuis R12
+		str	r0, [r6]            ; Configurer le rapport cyclique avec la vitesse
+
+		ldr	r6, =PWM0CMPB        ; Charger l'adresse de PWM0CMPB
+		str	r0, [r6]            ; Copier le rapport cyclique dans PWM0CMPB (dépend de CMPA)
 	;Control PWM : active PWM Generator 0 (p1167): Enable+up/down + Enable counter debug mod
 		ldr	r6, =PWM0CTL 
 		ldr	r0, [r6]	
 		ORR	r0,	r0,	#0x07
 		str	r0,	[r6]
-
 	;;-----------PWM2 pour moteur 2 connecté à PH0
 	;;PWM1block produit PWM2 et PWM3 output
 		;;Config Modes PWM2 + mode GenA + mode GenB
@@ -197,7 +178,8 @@ MOTEUR_INIT
 		str	r0,[r6]
 		
 		ldr	r6, =PWM1CMPA ;Valeur rapport cyclique : pour 10% => 1C2h si clock = 0F42400
-		mov	r0,	#VITESSE
+		mov	r0, r12             ; Charger la vitesse actuelle depuis R12
+		
 		str	r0, [r6]  ;*(int *)(0x40028000+0x058)=0x01C2;
 		
 		ldr	r6, =PWM1CMPB ;PWM0CMPB recoit meme valeur. (CMPA depend du rapport cyclique)
@@ -250,7 +232,6 @@ MOTEUR_INIT
 		str	r0,[r6]		
 		
 		BX	LR	; FIN du sous programme d'init.
-
 ;Enable PWM0 (bit 0) et PWM2 (bit 2) p1145 
 ;Attention ici c'est les sorties PWM0 et PWM2
 ;qu'on controle, pas les blocks PWM0 et PWM1!!!
@@ -261,56 +242,48 @@ MOTEUR_DROIT_ON
 		orr r0,	#0x01 ;bit 0 à 1
 		str	r0,	[r6]
 		BX	LR
-
 MOTEUR_DROIT_OFF 
 		ldr	r6,	=PWMENABLE
 		ldr r0,	[r6]
 		and	r0,	#0x0E	;bit 0 à 0
 		str	r0,	[r6]
 		BX	LR
-
 MOTEUR_GAUCHE_ON
 		ldr	r6,	=PWMENABLE
 		ldr	r0, [r6]
 		orr	r0,	#0x04	;bit 2 à 1
 		str	r0,	[r6]
 		BX	LR
-
 MOTEUR_GAUCHE_OFF
 		ldr	r6,	=PWMENABLE
 		ldr	r0,	[r6]
 		and	r0,	#0x0B	;bit 2 à 0
 		str	r0,	[r6]
 		BX	LR
-
 MOTEUR_DROIT_ARRIERE
 		;Inverse Direction (GPIO_D1)
 		ldr	r6, =(GPIODATA_D+(GPIO_1<<2)) 
 		mov	r0, #0
 		str	r0,[r6]
 		BX	LR
-
 MOTEUR_DROIT_AVANT
 		;Inverse Direction (GPIO_D1)
 		ldr	r6, =(GPIODATA_D+(GPIO_1<<2)) 
 		mov	r0, #2
 		str	r0,[r6]
 		BX	LR
-
 MOTEUR_GAUCHE_ARRIERE
 		;Inverse Direction (GPIO_D1)
 		ldr	r6, =(GPIODATA_H+(GPIO_1<<2)) 
 		mov	r0, #2 ; contraire du moteur Droit
 		str	r0,[r6]
 		BX	LR		
-
 MOTEUR_GAUCHE_AVANT
 		;Inverse Direction (GPIO_D1)
 		ldr	r6, =(GPIODATA_H+(GPIO_1<<2)) 
 		mov	r0, #0
 		str	r0,[r6]
 		BX	LR		
-
 MOTEUR_DROIT_INVERSE
 		;Inverse Direction (GPIO_D1)
 		ldr	r6, =(GPIODATA_D+(GPIO_1<<2)) 
@@ -318,7 +291,6 @@ MOTEUR_DROIT_INVERSE
 		EOR	r0, r1, #GPIO_1
 		str	r0,[r6]
 		BX	LR
-
 MOTEUR_GAUCHE_INVERSE
 		;Inverse Direction (GPIO_D1)
 		ldr	r6, =(GPIODATA_H+(GPIO_1<<2)) 
@@ -326,5 +298,4 @@ MOTEUR_GAUCHE_INVERSE
 		EOR	r0, r1, #GPIO_1
 		str	r0,[r6]
 		BX	LR
-
 		END
